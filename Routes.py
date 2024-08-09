cc035932b12425f5962175e4f9995f2add209f65
VictorJoss <vramirezgarcia64@correo.unicordoba.edu.co>
2024-08-09 10:40:43 -0500

    chore: Rename PRODUCTS route to match endpoint name

--- Routes.py
+++ Routes.py
@@ -3,6 +3,6 @@ import re
 class Routes:
     MENU = "/menu"
     CREATE_ORDER = "/create_order"
-    PRODUCT = "/products"
+    PRODUCTS = "/products"
     GET_MY_ORDERS = "/get_my_orders"
     REGISTER_ORDER = re.compile(r'^Order:.*')
