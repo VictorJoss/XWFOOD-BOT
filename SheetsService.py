7b2792d640b0769223966085c58314e164df7a29
VictorJoss <vramirezgarcia64@correo.unicordoba.edu.co>
2024-08-09 10:41:26 -0500

    chore: Update SheetsService.py to use google.oauth2.service_account.Credentials

--- SheetsService.py
+++ SheetsService.py
@@ -1,14 +1,15 @@
 import gspread
 from gspread.spreadsheet import Spreadsheet
-from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
 
 class SheetsService:
     def __init__(self, credentials_path: str) -> None:
 
-        scopes = ["https: //www.googleapis.com/auth/spreadsheets"]
-        credentials = ServiceAccountCredentials.from_service_account_file(credentials_path, scopes)
+        scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+        credentials = Credentials.from_service_account_file(credentials_path, scopes = scopes)
         self.client = gspread.authorize(credentials)
 
-    def get_sheet_by_id(sheet_id: str) -> Spreadsheet:
+    def get_sheet_by_id(self, sheet_id: str) -> Spreadsheet:
         sheet = self.client.open_by_key(sheet_id)
         return sheet
+    
